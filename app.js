5b08328a161a5aa967486c86c44bb9062f7ee6a8
mmhughes2 <kenhughes925@gmail.com>
2026-03-26 10:01:02 -0400

    added mongodb connection

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
-var http = require("http");
-const port = process.env.PORT || 3001;
+const mongoose = require("mongoose");
 
-http
-  .createServer(function (req, res) {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.end("Hello Mackenzie");
-  })
-  .listen(port);
+//testdb is name of database, it will automatically make it
+mongoose
+  .connect("mongodb+srv://mhughes:u95jsL5oHSZ62tLo@shelfspace.myez2x0.mongodb.net/")
+  .then(() => console.log("Connected to mongodb..."))
+  .catch((err) => console.error("could not connect ot mongodb...", err));
+
+const schema = new mongoose.Schema({
+  name: String,
+});
+
+async function createMessage() {
+  const result = await message.save();
+  console.log(result);
+}
+
+//this creates a Message class in our app
+const Message = mongoose.model("Message", schema);
+const message = new Message({
+  name: "Hello World",
+});
+
+createMessage();
