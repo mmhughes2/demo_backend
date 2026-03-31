b1ff1f376ab5f4a1e136c23a7505bbd6b833133a
mmhughes2 <kenhughes925@gmail.com>
2026-03-31 09:24:59 -0400

    updated backend for get request

--- app.js
+++ app.js
@@ -1,24 +1,111 @@
-const mongoose = require("mongoose");
+const express = require("express");
+const cors = require("cors");
+const multer = require("multer");
+const app = express();
+app.use(express.static("public"));
+app.use(express.json());
+app.use(cors());
 
-//testdb is name of database, it will automatically make it
-mongoose
-  .connect("mongodb+srv://mhughes:u95jsL5oHSZ62tLo@shelfspace.myez2x0.mongodb.net/")
-  .then(() => console.log("Connected to mongodb..."))
-  .catch((err) => console.error("could not connect ot mongodb...", err));
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, "./public/images/");
+    },
+    filename: (req, file, cb) => {
+      cb(null, file.originalname);
+    },
+  });
+  
+const upload = multer({ storage: storage });
 
-const schema = new mongoose.Schema({
-  name: String,
+  let books = [
+  {
+    "_id": 1,
+    "title": "The Midnight Library",
+    "author": "Matt Haig",
+    "genre": "Contemporary Fantasy",
+    "page_count": 304,
+    "rating": 4.2,
+    "features": [
+      "book club favorite",
+      "reflective storytelling"
+    ],
+    "main_image": "midnight-library.jpg",
+    "formats": [
+      {
+        "type": "Hardcover",
+        "isbn": "9780525559474"
+      },
+      {
+        "type": "Paperback",
+        "isbn": "9780143135135"
+      }
+    ]
+  },
+  {
+    "_id": 2,
+    "title": "Project Hail Mary",
+    "author": "Andy Weir",
+    "genre": "Science Fiction",
+    "page_count": 496,
+    "rating": 4.7,
+    "features": [
+      "fast-paced adventure",
+      "first contact story"
+    ],
+    "main_image": "project-hail-mary.jpg",
+    "formats": [
+      {
+        "type": "Hardcover",
+        "isbn": "9780593135204"
+      },
+      {
+        "type": "Audiobook",
+        "isbn": "9780593455203"
+      }
+    ]
+  },
+  {
+    "_id": 3,
+    "title": "Tomorrow, and Tomorrow, and Tomorrow",
+    "author": "Gabrielle Zevin",
+    "genre": "Literary Fiction",
+    "page_count": 416,
+    "rating": 4.3,
+    "features": [
+      "character-driven",
+      "gaming industry backdrop",
+      "multi-decade timeline"
+    ],
+    "main_image": "tomorrow-and-tomorrow.jpg",
+    "formats": [
+      {
+        "type": "Hardcover",
+        "isbn": "9780593321201"
+      },
+      {
+        "type": "eBook",
+        "isbn": "9780593321218"
+      }
+    ]
+  }
+]
+
+app.get("/api/books",(req,res)=>{
+  res.send(books);
 });
 
-async function createMessage() {
-  const result = await message.save();
-  console.log(result);
-}
+app.get("/api/books/:id", (req,res)=>{
+  const requestedBook = books.find((b) => b._id === parseInt(req.params.id, 10));
 
-//this creates a Message class in our app
-const Message = mongoose.model("Message", schema);
-const message = new Message({
-  name: "Hello World",
+  if (!requestedBook) {
+    return res.status(404).send({ message: "Book not found" });
+  }
+
+  res.send(requestedBook);
 });
 
-createMessage();
+// listen for incoming requests
+app.listen(3001, ()=>{
+  console.log("Server is up and running");
+  
+});
