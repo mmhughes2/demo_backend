4e3c71532af5c94f94376e01cfe1e761457969ab
mmhughes2 <kenhughes925@gmail.com>
2026-04-09 09:21:46 -0400

    adding request post features

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const multer = require("multer");
+const Joi = require("joi");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -61,6 +63,7 @@ const books = [
 app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
+
 
 app.get("/api", (req, res) => {
   res.send({
